e7f20ddb163965815d9a96716ee8017a51d20e6a
unknown <bpkai756@gmail.com>
2022-08-03 04:41:04 -0700

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,10 @@
+let curent result = 0;
+
+
+const userinput = 100;
+userfirstinput = 120;
+let operator = "+";
+let usersecondinput = 220;
+
+currentresult = userfirstinput + usersecondinput;
+let result_description ='${userfirstinput} ${operator} ${usersecondinput}'
